4b15be20eb79e4f800d6c8ac080358c6fb48129b
mychan8 <mychan8@protonmail.com>
2021-09-04 13:02:07 -0300

    ...

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,8 @@
 @extends('layouts.template')
+
+@section('title')
+	<div>
+		<h5 style="color: gray; text-align:center;">¡Bienvenidos a</h5>
+		<h1 style="color: red; text-align: center;">Viandas Pop!</h1>
+	</div>
+@endsection
--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -10,69 +10,73 @@
     <meta name="keywords"  content="">
     ----->
     <style type="text/css">
-	.sidebar {
-	  height: 100%; /* 100% Full-height */
-	  width: 0; /* 0 width - change this with JavaScript */
-	  position: fixed; /* Stay in place */
-	  z-index: 1; /* Stay on top */
-	  top: 0;
-	  left: 0;
-	  background-color: #111; /* Black*/
-	  overflow-x: hidden; /* Disable horizontal scroll */
-	  padding-top: 60px; /* Place content 60px from the top */
-	  transition: 0.5s; /* 0.5 second transition effect to slide in the sidebar */
-	}
+		.sidebar {
+		  height: 100%; /* 100% Full-height */
+		  width: 0; /* 0 width - change this with JavaScript */
+		  position: fixed; /* Stay in place */
+		  z-index: 1; /* Stay on top */
+		  top: 0;
+		  left: 0;
+		  background-color: #111; /* Black*/
+		  overflow-x: hidden; /* Disable horizontal scroll */
+		  padding-top: 60px; /* Place content 60px from the top */
+		  transition: 0.5s; /* 0.5 second transition effect to slide in the sidebar */
+		}
 
-	/* The sidebar links */
-	.sidebar a {
-	  padding: 8px 8px 8px 32px;
-	  text-decoration: none;
-	  font-size: 25px;
-	  color: #818181;
-	  display: block;
-	  transition: 0.3s;
-	}
+		/* The sidebar links */
+		.sidebar a {
+		  padding: 8px 8px 8px 32px;
+		  text-decoration: none;
+		  font-size: 25px;
+		  color: #818181;
+		  display: block;
+		  transition: 0.3s;
+		}
 
-	/* When you mouse over the navigation links, change their color */
-	.sidebar a:hover {
-	  color: #f1f1f1;
-	}
+		/* When you mouse over the navigation links, change their color */
+		.sidebar a:hover {
+		  color: #f1f1f1;
+		}
 
-	/* Position and style the close button (top right corner) */
-	.sidebar .closebtn {
-	  position: absolute;
-	  top: 0;
-	  right: 25px;
-	  font-size: 36px;
-	  margin-left: 50px;
-	}
+		/* Position and style the close button (top right corner) */
+		.sidebar .closebtn {
+		  position: absolute;
+		  top: 0;
+		  right: 25px;
+		  font-size: 36px;
+		  margin-left: 50px;
+		}
 
-	/* The button used to open the sidebar */
-	.openbtn {
-	  font-size: 20px;
-	  cursor: pointer;
-	  background-color: #111;
-	  color: white;
-	  padding: 10px 15px;
-	  border: none;
-	}
+		/* The button used to open the sidebar */
+		.openbtn {
+		  font-size: 20px;
+		  cursor: pointer;
+		  background-color: #111;
+		  color: white;
+		  padding: 10px 15px;
+		  border: none;
+		}
 
-	.openbtn:hover {
-	  background-color: #444;
-	}
+		.openbtn:hover {
+		  background-color: #444;
+		}
 
-	/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
-	#main {
-	  transition: margin-left .5s; /* If you want a transition effect */
-	  padding: 20px;
-	}
+		/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+		#main {
+		  transition: margin-left .5s; /* If you want a transition effect */
+		  padding: 20px;
+		}
 
-	/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
-	@media screen and (max-height: 450px) {
-	  .sidebar {padding-top: 15px;}
-	  .sidebar a {font-size: 18px;}
-	}
+		/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+		@media screen and (max-height: 450px) {
+		  .sidebar {padding-top: 15px;}
+		  .sidebar a {font-size: 18px;}
+		}
 	</style>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="SCSS/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
 </head>
 <body style="font-family: 'Lato', sans-serif;'">
 
@@ -80,7 +84,7 @@
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
   <a href="#">Nosotros</a>
   <a href="#">Servicios</a>
-  <a href="#">Ubicación</a>
+  <a href="#ubicacion">Ubicación</a>
   <a href="#">Contacto</a>
 </div>
 
@@ -88,11 +92,33 @@
   <button class="openbtn" onclick="openNav()">☰</button>
 </div>
 
-<div>
-	<h1 style="color: red; text-align: center;">Viandas Pop!</h1>
+@yield('title')
+<div style="font-size: medium; margin: 15px">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </div>
-
 <script type="text/javascript" src="js/index.js"></script>
-   
+<div style="color: white; background: black;">
+	<div id='ubicacion' style="vertical-align: center; margin: 5px;">
+		<h5 style="margin: 5px;">Ubicación</h5>
+		<iframe width="250" height="250" id="gmap_canvas" src="https://maps.google.com/maps?q=buenos%20aires&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+	</div>
+</div>
 </body>
 </html> 
